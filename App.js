14d6ddea891890500beb7f1442b2a6f001883de3
Sanket Patil <sanketpatil@nimapinfotech.com>
2023-06-17 18:20:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Text, TextInput, View } from "react-native"
+import Login from "./components/Login";
+
+
+function App (){
+  return (
+    <View>
+      
+      <Login/>
+      
+    </View>
+  )
+}
+export default App
+
+
+
